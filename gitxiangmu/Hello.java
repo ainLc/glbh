1f1194af68106c1bccec867df0aaf0e2393b9833
aini <veer_ai@sina.com>
2020-07-10 16:16:33 +0800

    第二次操作

--- gitxiangmu/Hello.java
+++
@@ -1,9 +0,0 @@
-public class Hello
-{
-	public static void main (String args[]){
-
-      System.out.println("ssssss1");
-	      System.out.println("ssssss2");   
-		  System.out.println("ssssss3");
-	}
-}
